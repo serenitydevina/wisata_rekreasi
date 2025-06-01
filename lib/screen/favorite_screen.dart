64b90bc8ef6890e2322a4542d0c0399572a577c5
Fellycia Caroline <165134673+FellyciaCaroline@users.noreply.github.com>
2025-06-01 14:10:53 +0700

    update template favorite screen

--- lib/screen/favorite_screen.dart
+++ lib/screen/favorite_screen.dart
@@ -1,4 +1,37 @@
+// import 'package:flutter/material.dart';
+
+// class FavoriteScreen extends StatefulWidget {
+//   const FavoriteScreen({super.key});
+
+//   @override
+//   State<FavoriteScreen> createState() => _FavoriteScreenState();
+// }
+
+// class _FavoriteScreenState extends State<FavoriteScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+//        appBar: AppBar(
+//         backgroundColor:
+//         //  const Color.fromRGBO(207, 224, 195, 1),
+//         Theme.of(context).appBarTheme.backgroundColor,
+//         // leading: IconButton(
+//         //   onPressed: () {
+//         //     Navigator.pop(context);
+//         //   },
+//         //   icon: const Icon(Icons.arrow_back, color: Colors.black),
+//         // ),
+//         title: Text('Favorite', style: Theme.of(context).textTheme.bodyLarge),
+//         ), 
+//       );
+//   }
+// }
+
+import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
@@ -8,22 +41,96 @@ class FavoriteScreen extends StatefulWidget {
 }
 
 class _FavoriteScreenState extends State<FavoriteScreen> {
+  final user = FirebaseAuth.instance.currentUser;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-       appBar: AppBar(
-        backgroundColor:
-        //  const Color.fromRGBO(207, 224, 195, 1),
-        Theme.of(context).appBarTheme.backgroundColor,
-        // leading: IconButton(
-        //   onPressed: () {
-        //     Navigator.pop(context);
-        //   },
-        //   icon: const Icon(Icons.arrow_back, color: Colors.black),
-        // ),
-        title: Text('Favorite', style: Theme.of(context).textTheme.bodyLarge),
-        ), 
-      );
+      backgroundColor:  Theme.of(context).scaffoldBackgroundColor,
+      appBar: AppBar(
+        title: Text('Favorite', style: Theme.of(context).textTheme.bodyLarge ,),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('favorites')
+            .where('userId', isEqualTo: user!.uid)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          final favoriteDocs = snapshot.data!.docs;
+
+          if (favoriteDocs.isEmpty) {
+            return const Center(child: Text('Belum ada wisata favorit.'));
+          }
+
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GridView.builder(
+              itemCount: favoriteDocs.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // 2 kolom
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                childAspectRatio: 3 / 4,
+              ),
+              itemBuilder: (context, index) {
+                final doc = favoriteDocs[index];
+                final data = doc.data() as Map<String, dynamic>;
+
+                final imageBase64 = data['gambarUrl'];
+                final bytes = base64Decode(imageBase64);
+
+                return Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: const BorderRadius.vertical(top: Radius.circular(12)),
+                        child: Image.memory(
+                          bytes,
+                          height: 100,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          data['nama'] ?? '',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                      const Spacer(),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: IconButton(
+                          icon: const Icon(Icons.favorite, color: Colors.red),
+                          onPressed: () async {
+                            await FirebaseFirestore.instance
+                                .collection('favorites')
+                                .doc(doc.id)
+                                .delete();
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      ),
+    );
   }
 }
